7a15508f5ed9ebda8e9fd930a100a560eacaa64c
Dini <DDeSilva@smail.pembinatrails.ca>
2024-11-28 15:12:44 -0600

    testedstuff

---
+++ chaos.sh
@@ -0,0 +1,37 @@
+echo "please type a number in between 1-10 (no decimals nor fractions"
+read num
+if [ "$num" == "1" ];then
+	echo "your bad at typing numbers"
+elif [ "$num" == "2" ];then
+	echo "your so lazy, you typed your own IQ into the chat. How old are you? 2?"
+elif [ "$num" == "3" ];then
+	echo "i bet thats how many brain cells you have while talking to a girl."
+elif [ "$num" == "4" ];then
+	echo "Hey! You cant choose four, you already typed that in 'how to get a adopted'"
+elif [ "$num" == "5" ];then
+	echo "so, you have chosen 5 huh? So you know that 10-5 is 5 huh? Didnt think so."
+elif [ "$num" == "6" ];then
+	echo "cool"
+elif [ "$num" == "7" ];then
+    echo "this is the number seven upside down"
+    sleep 5
+    echo "you fell for it (dark joke)"
+    sleep 5
+    echo "I just wrote dark joke to make you think for 5 sec straight on a chair while Dini is just staring at you"
+elif [ "$num" == "8" ];then
+    echo "wow, seven huh."
+    sleep 2
+    echo "your an absolute faliure"
+elif [ "$num" == "9" ];then
+    echo "I know someone who was nine, hes dead now"
+elif [ "$num" == "10" ];then
+	echo "I am an organization reaserching on very unintelligent people, if your hearing this..."
+	sleep 2
+	echo "RUN YOUR THE IDIOT UNDER REASERCH.... RUUUUUUUUUUUUUUUUUUUUUUNNNNNNNNNNNN"
+else
+	echo "dont give me that poop, Im not in the mood."
+	sleep 2
+	echo "you know what? Im going to make you unable to type"
+	sleep 1233
+	echo "you have waited 1233 seconds to here this message"
+fi
